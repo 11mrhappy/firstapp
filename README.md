69233d183d9bb5d2a3750d92050f47ded40a0b8f
11mrhappy <nekomamiremy@gmail.com>
2021-02-16 21:42:18 +0900

    画面遷移とルーティング

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 stateless widget は状態をもたず widgetが作られた時点で設定したプロパティを変更できない
 
 stateful widgetは stateful widgetクラス と stateクラスで作られる
+
+画面遷移時、onpressed内に navigator.push()とし、()内、つまりパラメータには1つ目にcontext、2つ目に遷移先を書く
+MaterialPageRoute(builder: (context) => SecondRoute()) 2つ目はこのようになる。
+逆に、画面を戻るときは移動先のページに、pushではなくpopとかくだけで戻れる。パラメータはcontextのみでよい。
+pushは子フォルダ、popは親フォルダに移動し、現在のフォルダの画面が表示されているようなイメージ
+
+また、ルーティングを関数作成時点で書いておくと今後が楽。こんな感じ
+initialRoute: '/first',
+routes: <String, WidgetBuilder> {
+  '/first':(BuildContext context) => FirstRoute(),
+  '/second':(BuildContext context) => SecondRoute(),
+}
+そして、この場合の画面遷移は、pushではなくpushNamed(context, '/second'); みたいに書く
+ただ、戻るときは普通にpopメソッドでOK
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,63 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello World')
+void main() => runApp(MaterialApp(
+    initialRoute: '/first',
+    routes: <String, WidgetBuilder> {
+      '/first':(BuildContext context) => FirstRoute(),
+      '/second':(BuildContext context) => SecondRoute(),
+    }
+  )
+);
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first route'),
       ),
-    ),
-  );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+              child: Text('push here',
+                style: TextStyle(fontSize: 20),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('second route'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('push here',
+                style: TextStyle(fontSize: 20),
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
